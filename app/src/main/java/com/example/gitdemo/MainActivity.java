603b9f2b43b97d84d702840a878b6d0af64cde2a
eladmaa <89908683+eladmaa@users.noreply.github.com>
2025-11-05 10:06:17 +0200

    first commit

---
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.gitdemo;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    lrvoldgfslkfdgzlnk;
+    }
+}
